4e0d2e05b249e9849fe040e5f007c2e06abccf3c
Ilya Petrov <gtgtr3000@gmail.com>
2023-02-24 17:57:55 +0300

    wip

--- tools/containers/vector.cpp
+++ tools/containers/vector.cpp
@@ -1,6 +1,11 @@
 #include "vector.hpp"
 
-namespace tools {
-  namespace containers {
-  } // tools
-} // containers
+namespace tools::containers {
+
+  template<typename T_v>
+  vector<T_v>::vector() {
+    data_ = nullptr;
+  }
+
+
+} // tools::containers
--- tools/containers/vector.hpp
+++ tools/containers/vector.hpp
@@ -1,13 +1,36 @@
+#include <initializer_list>
+
 #pragma once
 
-namespace tools {
-  namespace containers {
+namespace tools::containers {
 
-    class vector {
+  template<typename T_v>
+  class vector {
+  public:
 
-    };
+    // constructors
+    vector();
 
-  } // tools
-} // containers
+    vector(size_t size);
+
+    vector(size_t size, const T_v& default_value);
+
+    vector(const std::initializer_list <T_v> &initializer_list);
+
+    // methods
+    void push_back(const T_v& val);
+
+    void resize(size_t size) {
+
+    }
+  private:
+    T_v* data_;
+    size_t size_;
+    size_t capacity_;
+  };
+
+
+
+} // tools::containers
 
 
